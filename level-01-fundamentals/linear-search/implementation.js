f4a51d039cd4a7e5bba6a6489417ab58227fafa6
MiroDev20 <mirodev20@gmail.com>
2026-07-28 13:39:16 -0500

    docs: document common mistakes in linear search implementation

--- level-01-fundamentals/linear-search/implementation.js
+++ level-01-fundamentals/linear-search/implementation.js
@@ -2,13 +2,13 @@ function linearSearch (target, array)
 {
     let result;
 
-    if (!typeof target === 'number')
+    if (!typeof target === 'number') // 🔥 FIXME: fix type validation
     {
         result = `A number was expected, but it was obtained ${typeof target}`
     }
     else
     {
-        if (!array instanceof Array)
+        if (!array instanceof Array) // 🔥 FIXME: fix type validation
         {
             result = `An array was expected, but it was obtained ${typeof array}`
         }
@@ -18,13 +18,13 @@ function linearSearch (target, array)
             {
                 if (array[i] === target)
                 {
-                    result = array.indexOf(array[i])
+                    result = array.indexOf(array[i]) // 🔧 REFACTOR: innecesary use of `.indexOf(array[i])`
                 }
             }
         }
     }
 
-    return result || -1
+    return result || -1 // 🔥 FIXME: fix return. When result = 0 returns -1 because consider it falsy
 }
 
-console.log(linearSearch(8, [1, 5, 2, 7, 5, 8, 0, 3]))
+console.log(linearSearch(1, [1, 5, 2, 7, 5, 8, 0, 3]))
